ceb01f75b1d053dbedb2ce42a5d0001c94320982
Javier Gongora <jgongora@gmail.com>
2022-09-22 16:02:01 -0700

    Create FeaturedCollections and fetch them from the Storefront API

---
+++ src/components/FeaturedCollections.server.jsx
@@ -0,0 +1,43 @@
+import { Link, Image, gql, useShopQuery, CacheLong } from '@shopify/hydrogen'
+
+export default function FeaturedCollections() {
+  const {
+    data: { collections },
+  } = useShopQuery({
+    query: QUERY,
+    cache: CacheLong(),
+  })
+
+  return (
+    <section className="w-full gap-4 md:gap-8 grid p-6 md:p-8 lg:p-12">
+      <h2 className="whitespace-pre-wrap max-w-prose font-bold text-lead">
+        Collections
+      </h2>
+      <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-1 false sm:grid-cols-3 false false">
+        {collections.nodes.map((collection) => {
+          return (
+            <Link key={collection.id} to={`/collections/${collection.handle}`}>
+              <div className="grid gap-4">
+                <h2 className="whitespace-pre-wrap max-w-prose font-medium text-copy">
+                  {collection.title}
+                </h2>
+              </div>
+            </Link>
+          )
+        })}
+      </div>
+    </section>
+  )
+}
+
+const QUERY = gql`
+  query FeaturedCollections {
+    collections(first: 3, query: "collection_type:smart", sortKey: UPDATED_AT) {
+      nodes {
+        id
+        title
+        handle
+      }
+    }
+  }
+`
