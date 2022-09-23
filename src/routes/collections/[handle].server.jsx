04e3a0789dc29390ea7275261e494339971c688a
Javier Gongora <jgongora@gmail.com>
2022-09-22 17:17:43 -0700

    Create collections route

---
+++ src/routes/collections/[handle].server.jsx
@@ -0,0 +1,13 @@
+import { useRouteParams } from '@shopify/hydrogen'
+import { Layout } from '../../components/Layout.server'
+
+export default function Collection() {
+  const { handle } = useRouteParams()
+  return (
+    <Layout>
+      <section className="p-6 md:p-8 lg:p-12">
+        This will be the collection page for <strong>{handle}</strong>
+      </section>
+    </Layout>
+  )
+}
