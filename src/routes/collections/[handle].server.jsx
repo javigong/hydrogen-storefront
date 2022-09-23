793e2b761c60a2aa990945e3677c2e62b85ee545
Javier Gongora <jgongora@gmail.com>
2022-09-22 17:28:48 -0700

    Add query to collections using the handle route param

--- src/routes/collections/[handle].server.jsx
+++ src/routes/collections/[handle].server.jsx
@@ -1,13 +1,31 @@
-import { useRouteParams } from '@shopify/hydrogen'
+import { gql, useRouteParams, useShopQuery } from '@shopify/hydrogen'
 import { Layout } from '../../components/Layout.server'
 
 export default function Collection() {
   const { handle } = useRouteParams()
+  const {
+    data: { collection },
+  } = useShopQuery({
+    query: QUERY,
+    variables: {
+      handle,
+    },
+  })
+
   return (
     <Layout>
       <section className="p-6 md:p-8 lg:p-12">
-        This will be the collection page for <strong>{handle}</strong>
+        This will be the collection page for <strong>{collection.title}</strong>
       </section>
     </Layout>
   )
 }
+
+// Add a Graphql query to retrieve a collection by its handle
+const QUERY = gql`
+  query CollectionDetails($handle: String!) {
+    collection(handle: $handle) {
+      title
+    }
+  }
+`
