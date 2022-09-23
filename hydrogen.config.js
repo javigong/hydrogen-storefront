4dd681d2b7eb6728724e7971bb2972bdfa89110d
Javier Gongora <jgongora@gmail.com>
2022-09-23 12:12:52 -0700

    Update hydrogen.config.js

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -1,9 +1,12 @@
-import {defineConfig} from '@shopify/hydrogen/config';
+import { defineConfig } from '@shopify/hydrogen/config'
 
 export default defineConfig({
+  logger: {
+    showQueryTiming: true,
+  },
   shopify: {
     storeDomain: 'hydrogen-preview.myshopify.com',
     storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
     storefrontApiVersion: '2022-07',
   },
-});
+})
