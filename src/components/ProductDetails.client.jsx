95cbc0b18f8658fab3e31123000c161b6b515b0b
Javier Gongora <jgongora@gmail.com>
2022-09-23 12:48:06 -0700

    Add product gallery

--- src/components/ProductDetails.client.jsx
+++ src/components/ProductDetails.client.jsx
@@ -22,3 +22,55 @@ export default function ProductDetails({ product }) {
     </ProductOptionsProvider>
   )
 }
+
+function ProductGalley({ media }) {
+  if (!media.length) {
+    return null
+  }
+
+  return (
+    <div
+      className={`grid gap-4 overflow-x-scroll grid-flow-col md:grid-flow-row  md:p-0 md:overflow-x-auto md:grid-cols-2 w-screen md:w-full lg:col-span-2`}
+    >
+      {media.map((med, i) => {
+        let extraProps = {}
+
+        if (med.mediaContentType === 'MODEL_3D') {
+          extraProps = {
+            interactionPromptThreshold: '0',
+            ar: true,
+            loading: 'eager',
+            disableZoom: true,
+          }
+        }
+
+        const data = {
+          ...med,
+          image: {
+            ...med.image,
+            altText: med.alt || 'Product image',
+          },
+        }
+
+        return (
+          <div
+            className={`${
+              i % 3 === 0 ? 'md:col-span-2' : 'md:col-span-1'
+            } snap-center card-image bg-white aspect-square md:w-full w-[80vw] shadow-sm rounded`}
+            key={med.id || med.image.id}
+          >
+            <MediaFile
+              tabIndex="0"
+              className={`w-full h-full aspect-square object-cover`}
+              data={data}
+              options={{
+                crop: 'center',
+              }}
+              {...extraProps}
+            />
+          </div>
+        )
+      })}
+    </div>
+  )
+}
