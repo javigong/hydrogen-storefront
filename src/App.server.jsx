3dadfb35c5a2f785f176a9f7fc5ca9fd5a703d15
Javier Gongora <jgongora@gmail.com>
2022-09-23 13:04:59 -0700

    Wrap app in CartProvider

--- src/App.server.jsx
+++ src/App.server.jsx
@@ -1,18 +1,24 @@
-import React from 'react';
-import renderHydrogen from '@shopify/hydrogen/entry-server';
-import {Router, FileRoutes, ShopifyProvider} from '@shopify/hydrogen';
-import {Suspense} from 'react';
+import renderHydrogen from '@shopify/hydrogen/entry-server'
+import {
+  Router,
+  FileRoutes,
+  ShopifyProvider,
+  CartProvider,
+} from '@shopify/hydrogen'
+import { Suspense } from 'react'
 
-function App() {
+function App({ routes }) {
   return (
     <Suspense fallback={null}>
       <ShopifyProvider>
-        <Router>
-          <FileRoutes />
-        </Router>
+        <CartProvider>
+          <Router>
+            <FileRoutes routes={routes} />
+          </Router>
+        </CartProvider>
       </ShopifyProvider>
     </Suspense>
-  );
+  )
 }
 
-export default renderHydrogen(App);
+export default renderHydrogen(App)
